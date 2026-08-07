7780b8e93141fb73685352d082c8fe4113b0484c
zqj <582132116@qq.com>
2026-08-07 17:37:26 +0800

    fix: remove hardcoded Mapbox token

--- src/themes/classic/utils/const.ts
+++ src/themes/classic/utils/const.ts
@@ -1,10 +1,8 @@
 // Constants
 import { getStoredValue } from '../../../core/storage';
-const MAPBOX_TOKEN =
-  // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
-  // Instead, manually add a new token and apply URL restrictions.
-  // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJjbWYxdXR4YncwMTJtMm5zOTE4eTZpMGdtIn0.OnsXdwkZFztR8a5Ph_T-xg';
+// Mapbox tokens are public client-side values. Read one from the build
+// environment when configured, but never commit a token into the bundle.
+const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN || '';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
